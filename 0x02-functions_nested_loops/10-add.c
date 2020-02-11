981a91ef455976632c8582cbef86e1bce04577ff
FabianMosquera <rfmq9530@gmail.com>
2020-02-11 11:13:02 -0500

    add the number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - adds two integers
+ * @number1: enter the first number
+ * @number2: enter the second number
+ * Return: number1 + number2
+ */
+int add(int number1, int number2)
+{
+	return (number1 + number2);
+}
