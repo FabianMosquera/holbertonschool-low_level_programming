093cb448ccec9101e040be71c236a77ba460a7eb
FabianMosquera <rfmq9530@gmail.com>
2020-02-11 18:05:02 -0500

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,30 +10,29 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b)
-    {
-        if (a > c)
+	if (a > b)
+	{
+		if (a > c)
+			largest = a;
+		else
+			largest = c;
+	}
+	else if (b > a)
+	{
+		if (b > a)
+			largest = b;
+		else
+			largest = c;
+	}
+	else if (a < c)
+	{
 		largest = a;
+	}
 	else
+	{
 		largest = c;
-    }
-    else if (b > a)
-    {
-        if (b > a)
-		largest = b;
-	else
-		largest = c;
-    }
-    else if (a < c)
-    {
-        largest = a;
-    }
-    else
-    {
-	    largest = c;
-    }
-
-    return (largest);
+	}
+	return (largest);
 }
