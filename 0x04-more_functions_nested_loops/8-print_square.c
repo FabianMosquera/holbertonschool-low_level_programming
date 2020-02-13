814d4d1030aec38a431d3e9b2a644c1d5238696f
FabianMosquera <rfmq9530@gmail.com>
2020-02-13 14:53:23 -0500

    Prints squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,9 +9,9 @@ void print_square(int size)
 	int a;
 	int b;
 
-	for(a = 0; a < size; a++)
+	for (a = 0; a < size; a++)
 	{
-		for(b = 0; b < size; b++)
+		for (b = 0; b < size; b++)
 		{
 			_putchar('#');
 		}
