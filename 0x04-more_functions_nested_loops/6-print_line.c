4b1c576c0b4836e129be93d7714638464e61cba8
FabianMosquera <rfmq9530@gmail.com>
2020-02-13 13:33:19 -0500

    Print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_line - Prints the lines
+ * @n: values integer
+ * Return: integer
+ */
+void print_line(int n)
+{
+	while(--n >= 0)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
