cb60eea896b7b942397a1d213bda039ed69fc43c
FabianMosquera <rfmq9530@gmail.com>
2020-02-17 15:48:58 -0500

    swap the values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * swap_int - function that swaps the values
+ * @a: integer
+ * @b: integer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	if (a && b)
+	{
+		*a = 42;
+		*b = 98;
+	}
+}
