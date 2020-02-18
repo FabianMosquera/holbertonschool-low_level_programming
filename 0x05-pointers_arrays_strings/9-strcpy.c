84518013980aec27ac1d8eac6d9fb8b4985d693c
FabianMosquera <rfmq9530@gmail.com>
2020-02-18 17:54:11 -0500

    copies the string pointed

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * _strcpy - Function that copies the string
+ * @dest: char
+ * @src: char
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char a = 0;
+	char b = 0;
+
+	if (dest && src)
+	{
+		while (*(src + a))
+			++a;
+
+		if (src + a < dest || dest + a < src)
+		{
+			do {
+				*(dest + b++) = *src++;
+			} while (a--);
+		}
+	}
+	return (dest);
+}
