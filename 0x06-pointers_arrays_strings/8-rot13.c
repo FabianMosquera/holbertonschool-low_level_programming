d2c62a8ee2770e67ac55e0bc14758e7272981f94
FabianMosquera <rfmq9530@gmail.com>
2020-02-20 19:47:24 -0500

    Update

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -9,14 +9,16 @@ char *rot13(char *s)
 	int a, b;
 	char j[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char j1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char test;
 
 	for (a = 0; s[a] != '\0'; a++)
 	{
-		for (b = 0; b <= 51; b++)
+		for (b = 0; b <= 51 && test == 0; b++)
 		{
 			if (j[b] == s[a])
 			{
 				s[a] = j1[b];
+				test = 1;
 			}
 		}
 	}
