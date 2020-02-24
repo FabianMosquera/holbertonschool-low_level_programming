b9878a0cb9855295d02ed28455d6fd4d3c067052
FabianMosquera <rfmq9530@gmail.com>
2020-02-24 10:26:52 -0500

    copies memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _memcpy - Function that copies memory data
+ * @dest: char
+ * @src: char
+ * @n: unsigned int
+ * Return: char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = *src;
+		src++;
+	}
+	return (dest);
+}
