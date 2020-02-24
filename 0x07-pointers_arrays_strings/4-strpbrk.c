d4ec44a259f437b4a98d9fb86012463741245404
FabianMosquera <rfmq9530@gmail.com>
2020-02-24 12:22:47 -0500

    Function that searches a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strpbrk - Function that search a string
+ * @s: char
+ * @accept: char
+ * Return: char
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *a;
+
+	for ( ; *s; ++s)
+	{
+		for (a = accept; *a; a++)
+		{
+			if (*s == *a)
+				return(s);
+		}
+	}
+	return (s);
+}
