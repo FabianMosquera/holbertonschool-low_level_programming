7e6472453c7757b4bc99f0bb8201e8151a6c669f
FabianMosquera <rfmq9530@gmail.com>
2020-02-26 17:20:40 -0500

    Prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * _is_prime_number - Function that returns a prime number
+ * @n: int
+ * @m: int
+ * Return: int
+ */
+int _is_prime_number(int n, int m)
+{
+	if (n % m == 0)
+		return (0);
+	if (n < m * 2)
+		return (1);
+	return (_is_prime_number(n, m + 1));
+}
+/**
+ * _sqrt_recursion - Function that returns 1 is a palindrome and 0
+ * @n: int
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+	if (n < 4)
+		return (n > 1);
+	return (_is_prime_number(n, 2));
+}
