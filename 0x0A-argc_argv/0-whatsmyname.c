4cda4275d68feb850335a35f87e546a468ad3ed2
fabianmosquera <rfmq9530@hotmai.com>
2020-02-29 15:06:15 -0500

    Print name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,20 +4,20 @@
  *   * @str: the string to print
  *    *
  *     * Return: void
- *      */
+ *
+ */
 void _puts(char *str)
 {
-		while (*str)
-					_putchar(*str++);
-			_putchar('\n');
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
 }
 /**
- *  * main - print the name of the program
- *   * @argc: size of the argument vector
- *    * @argv: program name and arguments
- *     *
- *      * Return: Always 0
- *       */
+ * main - print the name of the program
+ * @argc: size of the argument vector
+ * @argv: program name and arguments
+ * Return: Always 0
+ */
 int main(int argc __attribute__ ((unused)), char *argv[])
 {
 		_puts(argv[0]);
