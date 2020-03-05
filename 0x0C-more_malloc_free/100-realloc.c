6fac03b6f8c18124f61fc0692657a59edcb69fdb
FabianMosquera <rfmq9530@gmail.com>
2020-03-05 16:40:42 -0500

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+/**
+ * _memcpy - Function that copies memory data
+ * @dest: char
+ * @src: char
+ * @n: unsigned int
+ * Return: char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = *src;
+		src++;
+	}
+	return (dest);
+}
+/**
+ * _realloc - Function that copies memory data
+ * @ptr: void
+ * @old_size: unsigned int
+ * @new_size: unsigned int
+ * Return: void
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new_ptr = malloc(new_size);
+	if (!new_ptr)
+		return (NULL);
+	_memcpy(new_ptr, ptr, old_size < new_size ? old_size : new_size);
+	free(ptr);
+
+	return (ptr);
+}
