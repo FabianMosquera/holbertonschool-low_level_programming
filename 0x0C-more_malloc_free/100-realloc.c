c5bce5408c32346f493ba49e4e0af36c94e30fd1
FabianMosquera <rfmq9530@gmail.com>
2020-03-05 16:53:49 -0500

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -43,5 +43,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	_memcpy(new_ptr, ptr, old_size < new_size ? old_size : new_size);
 	free(ptr);
 
-	return (ptr);
+	return (new_ptr);
 }
