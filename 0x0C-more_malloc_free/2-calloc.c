b23d6e9c6bba58bd9e25444e4711d1153bd30888
FabianMosquera <rfmq9530@gmail.com>
2020-03-05 14:39:50 -0500

    allocates in memory an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * _memset - Function that fills memory a constant byte
+ * @s: char
+ * @b: char
+ * @n: unsigned int
+ * Return: char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+/**
+ * _calloc - function that allocates memory for an array
+ * @nmemb: unsigned int
+ * @size: unsigned int
+ * Return: void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *g;
+
+	if (!(nmemb && size))
+		return (NULL);
+
+	g = malloc(sizeof(int) * (nmemb * size));
+	if (!g)
+		return (NULL);
+
+	_memset(g, 0, nmemb * size);
+	return (g);
+}
