8fc29c0c39fa58da1588ddffd6830850b6775a27
FabianMosquera <rfmq9530@gmail.com>
2020-03-09 16:58:40 -0500

    New Dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -43,7 +43,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (name)
 	{
 		d->name = _strdup(name);
-		if (!name)
+		if (!(d->name))
 		{
 			free(d);
 			return (NULL);
@@ -55,7 +55,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (owner)
 	{
 		d->owner = _strdup(owner);
-		if (!owner)
+		if (!d->owner)
 		{
 			free(d->name);
 			free(d);
