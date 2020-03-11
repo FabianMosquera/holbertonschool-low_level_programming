cab78e08282805eb34c45f6d64246e17b342ffde
FabianMosquera <rfmq9530@gmail.com>
2020-03-11 17:06:15 -0500

    main

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * main - This file should contain your main function only.
+ * @argc: integer
+ * @argv: char
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		puts("Error");
+		exit(98);
+	}
+
+	f = get_op_func(argv[2]);
+
+	if (!f)
+	{
+		puts("Error");
+		exit(99);
+	}
+	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
+
+	return (0);
+}
