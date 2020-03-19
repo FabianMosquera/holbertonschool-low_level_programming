4ae189baa78193475d51adcc353184eb77ed9ee0
fabianmosquera <rfmq9530@hotmai.com>
2020-03-19 14:12:17 -0500

    Library

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,27 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct list_l - Singly linked list
+ * @str: string
+ * @len: lenght of string
+ * @next: points to the next node
+ */
+typedef struct list_l
+{
+	char *str;
+	unsigned int len;
+	struct list_l *next;
+} list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif /* LIST_H */
