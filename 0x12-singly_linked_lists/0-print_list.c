47816a05697c485c94254230b275aba744bd4590
fabianmosquera <rfmq9530@hotmai.com>
2020-03-19 14:11:43 -0500

    Print all the elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_list - print list items
+ * @h: the list to print
+ * Return: size_t
+ */
+size_t print_list(const list_t *h)
+{
+	if (h)
+	{
+		if (h->str)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		return (print_list(h->next) + 1);
+	}
+	return (0);
+}
