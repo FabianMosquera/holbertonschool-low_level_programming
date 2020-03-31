9a9241f8c827188687075d584a5bf7fdcb5dfe05
fabianmosquera <rfmq9530@hotmai.com>
2020-03-31 16:27:30 -0500

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content)
-		b_written = write(var, text_context, _strlen(text_content));
+		b_written = write(var, text_content, _strlen(text_content));
 
 	close(var);
 
