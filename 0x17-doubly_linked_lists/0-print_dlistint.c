0bff4d3bb6f575a87d6a2e9a95c2473f019f4b7f
FabianMosquera <rfmq9530@gmail.com>
2020-05-02 16:00:23 +0000

    Print elements of struct

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,19 +1,18 @@
 #include "lists.h"
-
 /**
- * print_dlistint - function that print all the elements of a dlistint_t list
+ * print_dlistint - function that prints all the elements of a dlistint_t list
  * @h: header pointer
  * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t numbers = 0;
+	size_t cont = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		numbers++;
+		cont++;
 	}
-	return (numbers);
+	return (cont);
 }
